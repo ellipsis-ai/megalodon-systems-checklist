4a20727a6db362f0b075ee81ff2115c659511a01
Andrew Catton <>
2018-05-16 18:24:05 +0000

    initial import

---
+++ actions/intro/function.js
@@ -0,0 +1,14 @@
+function(ellipsis) {
+  const intro = `
+:shark: Hello, this is the Megalodon Systems Checklist reminder. 
+
+Anyone in this channel can answer the following three yes/no questions. Click below to begin.
+`;
+ellipsis.success(intro, {
+  choices: [{
+    label: "Start",
+    actionName: "run-checklist",
+    allowOthers: true
+  }]
+});
+}
---
+++ actions/run-checklist/function.js
@@ -0,0 +1,32 @@
+function(emitters, gutters, growLightsOnAuto, overheadLights, sandFilterGauge, ellipsis) {
+  const EllipsisApi = require('ellipsis-api');
+const api = new EllipsisApi(ellipsis);
+
+const channel = ellipsis.userInfo.messageInfo.channel;
+const user = ellipsis.userInfo.messageInfo.userId;
+const summary = `
+Megalodon checklist for channel <#${channel}> has been completed by <@${user}>:
+${checkFor(emitters)}   Emitters flowing
+${checkFor(gutters)}   Gutters draining
+${checkFor(growLightsOnAuto)}   Grow lights on auto
+${checkFor(overheadLights)}   Overhead room lights only on during production activities (M,W,F)
+${checkFor(sandFilterGauge)}   Both red and green arrows on sand filter gauge at 10
+`;
+
+const channels = ["farm-ops-announce"].filter(ea => ea != channel);
+api.say({ message: summary }).then(res => {
+  Promise.all(channels.map(postSummaryTo)).then(ellipsis.noResponse);                                 
+});
+
+function postSummaryTo(channel) {
+  return api.run({
+    actionName: "post-checklist-summary",
+    args: [{ name: "summary", value: summary }],
+    channel: channel
+  });
+}
+
+function checkFor(bool) {
+  return bool? ":white_check_mark:" : ":x:";
+}
+}
